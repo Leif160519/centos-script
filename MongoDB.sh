cd5fd01bfdc999f73020210004235c23ce8c2eb3
luofei <yzluofei@126.com>
2019-04-26 14:35:45 +0800

    升级mongodb版本到4.0

--- MongoDB.sh
+++ MongoDB.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo -e '\033[1;31m ********************************此脚本自动化安装MongoDB******************************** \033[0m'
 echo -e '\033[1;31m ********************************添加MongoDB源******************************** \033[0m'
-cat <<EOF > /etc/yum.repos.d/mongodb-org-3.6.repo
-[mongodb-org-3.6]
+cat <<EOF > /etc/yum.repos.d/mongodb-org-4.0.repo
+[mongodb-org-4.0]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/amazon/2013.03/mongodb-org/3.6/x86_64/
+baseurl=https://repo.mongodb.org/yum/amazon/2013.03/mongodb-org/4.0/x86_64/
 gpgcheck=1
 enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-3.6.asc
+gpgkey=https://www.mongodb.org/static/pgp/server-4.0.asc
 EOF
 echo -e '\033[1;31m 安装MongoDB \033[0m'
 yum install -y mongodb-org
