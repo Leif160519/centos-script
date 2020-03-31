a9a139efbbd067754470f073a321b1c52b325bf7
luofei <yzluofei@126.com>
2020-03-31 18:42:45 +0800

    zabbix-agent脚本新增重复尝试方法

--- zabbix/zabbix-agent.sh
+++ zabbix/zabbix-agent.sh
@@ -2,7 +2,21 @@
 echo -n "请输入zabbix服务器地址："
 read zabbix_server_ip
 rpm -Uvh https://repo.zabbix.com/zabbix/4.4/rhel/7/x86_64/zabbix-release-4.4-1.el7.noarch.rpm
-yum -y install zabbix-agent
+
+function install_agent(){
+  if [[ `yum list installed | grep zabbix-agent |wc -l` == 0 ]];then
+    yum -y clean all
+    yum -y install zabbix-agent
+    install_agent
+  else
+    echo -e '\033[1;32m zabbix-agent已经安装 \033[0m'
+  fi
+}
+
+install_agent
+
+
+
 
 echo "修改zabbix-agent配置文件"
 sed -i "s/Server=127.0.0.1/Server=${zabbix_server_ip}/g" /etc/zabbix/zabbix_agentd.conf
