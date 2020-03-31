977651064857c690c79cb00af3fc4662f29117ec
luofei <yzluofei@126.com>
2020-03-31 17:57:15 +0800

    更新zabbix-agent版本

--- zabbix/zabbix-agent.sh
+++ zabbix/zabbix-agent.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo -n "请输入zabbix服务器地址："
 read zabbix_server_ip
-rpm -i https://repo.zabbix.com/zabbix/4.0/rhel/7/x86_64/zabbix-release-4.0-1.el7.noarch.rpm
-yum -y install zabbix-agent-4.0.1
+rpm -Uvh https://repo.zabbix.com/zabbix/4.4/rhel/7/x86_64/zabbix-release-4.4-1.el7.noarch.rpm
+yum -y install zabbix-agent
 
 echo "修改zabbix-agent配置文件"
 sed -i "s/Server=127.0.0.1/Server=${zabbix_server_ip}/g" /etc/zabbix/zabbix_agentd.conf
