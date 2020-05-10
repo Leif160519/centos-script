7ef2462353b1a608d68091cfa2c688c0cb21ffe5
leif0207 <yzluofei@126.com>
2020-05-10 09:08:14 +0800

    更新关闭selinux和swap分区的写法

--- Base.sh
+++ Base.sh
@@ -149,17 +149,21 @@ echo -e '\033[1;31m 查看时间同步服务器运行状态 \033[0m'
 systemctl status ntpd
 echo -e '\033[1;31m ********************************************************************************** \033[0m'
 
-echo -e '\033[1;31m 4.关闭swap分区 \033[0m'
-swapoff -a
+echo -e '\033[1;31m 4.永久关闭swap分区（重启后生效） \033[0m'
+#swapoff -a 临时关闭
+sed -i "s/\/dev\/mapper\/centos-swap/#\/dev\/mapper\/centos-swap/g" /etc/fstab 
 echo -e '\033[1;31m 查看内存实用情况 \033[0m'
 free -m 
 echo -e '\033[1;31m ********************************************************************************** \033[0m'
 
 echo -e '\033[1;31m 5.关闭防火墙 \033[0m'
-echo -e '\033[1;31m 禁用SElinux \033[0m'
-setenforce 0
+echo -e '\033[1;31m 永久禁用SElinux （重启后生效）\033[0m'
+#setenforce 0 临时
 echo -e '\033[1;31m 修改 \033[1;33m /etc/selinux/config \033[0m 配置文件 \033[0m'
 sed -i "s/enforcing/disabled/g" /etc/selinux/config
+echo -e '\033[1;31m 查看selinux状态\033[0m 配置文件 \033[0m'
+sestatus
+getenforce
 echo -e '\033[1;31m 停止防火墙服务 \033[0m'
 systemctl stop firewalld
 echo -e '\033[1;31m 禁止防火墙开机自启 \033[0m'
@@ -212,4 +216,19 @@ echo -e '\033[1;31m ************************************************************
 echo -e '\033[1;32m系统初始化配置完成！\033[0m'
 echo -e "\033[1;31m 清除yum安装包 \033[0m"
 yum -y clean all
+
+function choose_reboot(){
+    echo -n "是否重启？(y or n)"
+    read choice
+    if [ ${choice} == "y" ];then
+        echo -e '\033[1;31m 你选择了重启 \033[0m'
+        reboot
+    elif [ ${choice} == "n" ];then
+        echo "你选择了不重启"
+    else
+        echo "输入有误，请重新输入"
+        choice
+    fi
+}
+choose_reboot
 exit
