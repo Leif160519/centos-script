f88af786bd10339ab3d12513588b43e4cc2a017d
leif0207 <yzluofei@126.com>
2020-07-05 15:29:17 +0800

    新增centos下nfs和samba的安装和配置

---
+++ nfs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+yum -y install nfs-utils
+echo -n "请输入需要共享的目录（不存在的会自动创建）："
+read share_dir
+mkdir -p ${share_dir}
+chmod -R 777 ${share_dir}
+echo -n "请输入允许访问nfs的IP段,如'192.168.1.0/24'（若允许所有主机都可访问，请输入'*'）："
+read ip
+# 添加共享目录
+cat <<EOF >> /etc/exports
+${share_dir} ${ip}(rw,sync,no_subtree_check,no_root_squash)
+EOF
+# 重启nfs服务
+systemctl restart nfs-kernel-server
+# 设置nfs开机自启动
+systemctl enable nfs-kernel-server
+
+browser_url="file://ip${share_dir}"
+windows_url="\\\ip\${share_dir}"
+
+echo "浏览器通过\"${browser_url}\",windows通过\"${windows_url}\"访问"
+exit
---
+++ samba.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+yum -y install samba
+echo -n "请输入需要共享的目录（不存在则会自动创建）："
+read share_dir
+mkdir -p ${share_dir}
+chmod -R 777 ${share_dir}
+echo -n "请输入共享的名称（别名）："
+read share_name
+echo "请设置共享的root密码"
+smbpasswd -a root
+
+# 添加共享目录
+cat <<EOF >> /etc/samba/smb.conf
+[${share_name}]
+   comment = ${share_name}
+   path = ${share_dir}
+   browseable = yes
+   writeable = yes
+   guest ok = yes
+   read only = no
+   valid user = root
+EOF
+
+# 重启samba
+systemctl restart smbd
+# 设置samba开机自启动
+systemctl enable smbd
+
+browser_url="file://ip/${share_name}/"
+windows_url="\\\ip\${share_name}"
+
+echo "浏览器通过\"${browser_url}\",windows通过\"${windows_url}\"访问"
+
+exit
