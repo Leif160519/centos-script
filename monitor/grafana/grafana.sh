5e873d5ca58f7d840f7c51c580b2f144b6e7e7f6
luofei1 <fei1.luo@genscript.com>
2020-07-13 17:25:01 +0800

    新增grafana安装脚本

---
+++ monitor/grafana/grafana.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# 设置grafana源
+cat <<EOF > /etc/yum.repos.d/grafana.repo
+[grafana]
+name=grafana
+baseurl=https://packages.grafana.com/oss/rpm
+repo_gpgcheck=1
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.grafana.com/gpg.key
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+EOF
+
+# 安装grafana
+yum -y install grafana
+
+# 方法2：直接下载安装包
+#wget -c https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-5.3.0-1.x86_64.rpm
+#yum -y install grafana-5.3.0-1.x86_64.rpm
+
+# 启动grafana
+systemctl start grafana-server
+# 查看服务状态
+systemctl status grafana-server
+# 设置开机自启
+systemctl enable grafana-server
+
+
+# 访问端口3000，默认用户名：admin，密码：admin
+echo "访问端口：3000，默认用户名：admin，密码：admin"
+# 插件目录：/var/lib/grafana/plugins
+echo "插件目录：/var/lib/grafana/plugins"
